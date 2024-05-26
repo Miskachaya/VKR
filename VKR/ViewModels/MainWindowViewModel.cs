52fd5242fe7a0c964b05b0abaec438f6fa31b9d6
maksb <maks.bodnyuk@mail.ru>
2024-05-26 22:27:26 +0800

    команда

--- VKR/ViewModels/MainWindowViewModel.cs
+++ VKR/ViewModels/MainWindowViewModel.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using VKR.Infrastructure.Commands;
 using VKR.ViewModels.Base;
 
 namespace VKR.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
         private string _Title="VKR";
         public string Title
         {
@@ -17,6 +21,30 @@ namespace VKR.ViewModels
             {
                 Set(ref _Title, value);
             }
+        }
+        #endregion
+
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand
+        {
+            get;
+        }
+        private void OnCloseApplicationCommandExecuted(object b)
+        {
+            Application.Current.Shutdown();
+        }
+        private bool CanCloseApplicationCommandExecuted(object b) => true; 
+        #endregion
+
+
+        #endregion
+        public MainWindowViewModel() 
+        {
+            #region Команды
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
+            #endregion
         }
     }
 }
