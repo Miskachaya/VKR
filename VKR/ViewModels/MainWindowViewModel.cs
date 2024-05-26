e14e7b64a5c8cf89f7f9a0b21dfb4209de5afe5b
maksb <maks.bodnyuk@mail.ru>
2024-05-26 20:10:27 +0800

    базовый класса команды

---
+++ VKR/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VKR.ViewModels.Base;
+
+namespace VKR.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title="VKR";
+        public string Title
+        {
+            get { return _Title; }
+            set
+            {
+                Set(ref _Title, value);
+            }
+        }
+    }
+}
